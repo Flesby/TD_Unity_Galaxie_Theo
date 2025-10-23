7e14bfadd71c3c71e115144ee09d202c18c6d1be
Flesby <35600646+Flesby@users.noreply.github.com>
2025-10-23 14:45:19 +0200

    commit terre

--- Td_Galaxie/Assets/Scripts/Orbits.cs
+++ Td_Galaxie/Assets/Scripts/Orbits.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class Orbits : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //Centre de l’orbite (ex: le Soleil)
+    public Transform pivot;
 
-    // Update is called once per frame
+    //Vitesse angulaire en degrés/seconde
+    public float angularSpeed = 20f;
+
+    //Rotation propre de la planète en degrés/seconde
+    public float selfRotationSpeed = 30f;
+
     void Update()
     {
-        
+        if (pivot != null)
+        {
+            // orbite autour du pivot (axe Y) 
+            transform.RotateAround(pivot.position, Vector3.up, angularSpeed * Time.deltaTime);
+        }
+        // rotation sur soi-même (axe Y) 
+        transform.Rotate(Vector3.up, selfRotationSpeed * Time.deltaTime, Space.Self);
     }
 }
